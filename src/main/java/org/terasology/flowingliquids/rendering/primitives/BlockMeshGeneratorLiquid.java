0c5fd0164a48010a0a09ee4f0f1a6bfa59c6b1e6
4Denthusiast <25589515+4Denthusiast@users.noreply.github.com>
2020-08-24 00:20:21 +0100

    Display sloped liquid surfaces
    Smooth out the surface height of the liquid meshes so that there's
    only one step per block level. Also don't render bubbles and
    simplify the appearance of waterfalls (removing the steps).

--- src/main/java/org/terasology/flowingliquids/rendering/primitives/BlockMeshGeneratorLiquid.java
+++ src/main/java/org/terasology/flowingliquids/rendering/primitives/BlockMeshGeneratorLiquid.java
@@ -61,16 +61,10 @@ public class BlockMeshGeneratorLiquid implements BlockMeshGenerator {
     
     @Override
     public void generateChunkMesh(ChunkView view, ChunkMesh chunkMesh, int x, int y, int z) {
-        Vector3i pos = new Vector3i(x,y,z);
-        float renderHeight = getRenderHeight(view, pos);
         
         ChunkVertexFlag vertexFlag = ChunkVertexFlag.NORMAL;
         if (block.isWater()) {
-            if (renderHeight == 1) {
-                vertexFlag = ChunkVertexFlag.WATER;
-            } else {
-                vertexFlag = ChunkVertexFlag.WATER_SURFACE;
-            }
+            vertexFlag = ChunkVertexFlag.WATER_SURFACE;
         }
         
         ChunkMesh.RenderType renderType = ChunkMesh.RenderType.TRANSLUCENT;
@@ -81,36 +75,59 @@ public class BlockMeshGeneratorLiquid implements BlockMeshGenerator {
         if (block.isWater()) {
             renderType = ChunkMesh.RenderType.WATER_AND_ICE;
         }
+
+        Vector3i pos = new Vector3i(x,y,z);
+        float[] renderHeight = getRenderHeight(view, pos);
+        boolean suppressed = view.getBlock(pos.x, pos.y+1, pos.z) == block; // Render it as full even though it actually isn't.
+        boolean full = suppressed || isFull(renderHeight);
         
         BlockAppearance appearance = block.getAppearance(null); //TODO: collect information the block wants, or avoid this entirely.
         for(Side side : Side.values()) {
             Vector3i adjacentPos = side.getAdjacentPos(pos);
             Block adjacentBlock = view.getBlock(adjacentPos);
-            float adjacentHeight = getRenderHeight(view, adjacentPos);
-            if(adjacentBlock != block) {
-                adjacentHeight = 0;
-            }
-            if(isSideVisibleForBlockTypes(adjacentBlock, adjacentHeight, block, renderHeight, side)) {
+            boolean adjacentSuppressed = view.getBlock(adjacentPos.x, adjacentPos.y+1, adjacentPos.z) == block;
+            if(isSideVisibleForBlockTypes(adjacentBlock, adjacentSuppressed, block, full, suppressed, side)) {
 //                Vector4f colorOffset = block.calcColorOffsetFor(BlockPart.fromSide(side), biome);
                 BlockMeshPart basePart = appearance.getPart(BlockPart.fromSide(side));
-                BlockMeshPart loweredPart = lowerPart(side, basePart, renderHeight, adjacentHeight);
+                BlockMeshPart loweredPart = lowerPart(side, basePart, renderHeight, suppressed, adjacentBlock == block);
                 loweredPart.appendTo(chunkMesh, x, y, z, renderType, vertexFlag);
             }
         }
     }
     
     // The height of the liquid block, as it is displayed.
-    private float getRenderHeight(ChunkView view, Vector3i pos) {
-        int height = LiquidData.getHeight((byte)view.getExtraData(flowIx, pos));
-        float renderHeight = height / (float) LiquidData.MAX_HEIGHT;
-        Block above = view.getBlock(Side.TOP.getAdjacentPos(pos));
-        if(!above.isLiquid() && !above.isFullSide(Side.BOTTOM)) {
-            renderHeight *= 0.9;
+    private float[] getRenderHeight(ChunkView view, Vector3i pos) {
+        float[] heights = new float[4];
+        int[] liquidCount = new int[4];
+        for (int x = -1; x <= 1; x++) {
+            for (int z = -1; z <= 1; z++) {
+                if (view.getBlock(pos.x+x, pos.y, pos.z+z) == block && view.getBlock(pos.x+x, pos.y+1, pos.z+z) != block) {
+                    int height = LiquidData.getHeight((byte)view.getExtraData(flowIx, pos.x+x, pos.y, pos.z+z));
+                    for (int i = 0; i < 4; i++) {
+                        if (((i < 2) ? (x <= 0) : (x >= 0)) && ((i % 2 == 0) ? (z <= 0) : (z >= 0))) {
+                            liquidCount[i]++;
+                            heights[i] += height / (float) LiquidData.MAX_HEIGHT;
+                        }
+                    }
+                }
+            }
         }
-        return renderHeight;
+        for (int i = 0; i < 4; i++) {
+            heights[i] /= liquidCount[i];
+        }
+        return heights;
+    }
+
+    private boolean isFull(float[] heights) {
+        for (float height : heights) {
+            if (height < 1) {
+                return false;
+            }
+        }
+        return true;
     }
     
-    private BlockMeshPart lowerPart(Side side, BlockMeshPart basePart, float height, float otherHeight) {
+    private BlockMeshPart lowerPart(Side side, BlockMeshPart basePart, float[] heights, boolean suppressed, boolean matches) {
         Vector3f[] vertices  = new Vector3f[basePart.size()];
         Vector3f[] normals   = new Vector3f[basePart.size()];
         Vector2f[] texCoords = new Vector2f[basePart.size()];
@@ -124,17 +141,16 @@ public class BlockMeshGeneratorLiquid implements BlockMeshGenerator {
             indices[i] = basePart.getIndex(i);
         }
         if(side == Side.TOP) {
-            for(int i=0; i<vertices.length; i++) {
+            for (int i=0; i<vertices.length; i++) {
+                float height = heights[(vertices[i].x > 0 ? 2 : 0) + (vertices[i].z > 0 ? 1 : 0)];
                 vertices[i].add(0, height-1f, 0);
             }
-        } else if(side != Side.BOTTOM) {
-            for(int i=0; i<vertices.length; i++) {
-                if(vertices[i].y > 0) {
-                    vertices[i].add(0, height-1f, 0);
-                    texCoords[i].add(0, -(height-1) * worldAtlas.getRelativeTileSize());
-                } else {
-                    vertices[i].add(0, otherHeight, 0);
-                    texCoords[i].add(0, -otherHeight * worldAtlas.getRelativeTileSize());
+        } else if(side != Side.BOTTOM && (!suppressed || matches)) {
+            for (int i=0; i<vertices.length; i++) {
+                if((vertices[i].y > 0) != suppressed) {
+                    float height = heights[(vertices[i].x > 0 ? 2 : 0) + (vertices[i].z > 0 ? 1 : 0)];
+                    vertices[i].add(0, height - (suppressed ? 0 : 1), 0);
+                    texCoords[i].add(0, -(height - (suppressed ? 0 : 1)) * worldAtlas.getRelativeTileSize());
                 }
             }
         }
@@ -154,12 +170,11 @@ public class BlockMeshGeneratorLiquid implements BlockMeshGenerator {
                 .toArray(org.joml.Vector2f[]::new);
     }
     
-    private boolean isSideVisibleForBlockTypes(Block blockToCheck, float heightToCheck, Block currentBlock, float currentHeight, Side side) {
-        if(side == Side.TOP && currentHeight < 1) {
+    private boolean isSideVisibleForBlockTypes(Block blockToCheck, boolean adjacentSuppressed, Block currentBlock, boolean full, boolean suppressed, Side side) {
+        if(side == Side.TOP && !full) {
             return true;
         } else if(blockToCheck == currentBlock) {
-            return !side.isVertical() && heightToCheck < currentHeight
-                || side == Side.BOTTOM && heightToCheck < 1;
+            return (side != Side.BOTTOM && side != Side.TOP && suppressed && !adjacentSuppressed);
         } else {
             return currentBlock.isWaving() != blockToCheck.isWaving()
                 || blockToCheck.getMeshGenerator() == null
