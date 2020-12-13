3dc0964725ec968f8f80f283dfdd9390607a7065
Michael Pollind <polli104@mail.chapman.edu>
2020-12-13 05:05:18 -0800

    feat(JOML): migrate LiquidDragSystem (#20)

--- src/main/java/org/terasology/flowingliquids/physics/LiquidDragSystem.java
+++ src/main/java/org/terasology/flowingliquids/physics/LiquidDragSystem.java
@@ -16,81 +16,77 @@
 
 package org.terasology.flowingliquids.physics;
 
-import java.math.RoundingMode;
-
+import org.joml.RoundingMode;
+import org.joml.Vector3f;
+import org.joml.Vector3i;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.terasology.entitySystem.entity.EntityManager;
 import org.terasology.entitySystem.entity.EntityRef;
 import org.terasology.entitySystem.systems.BaseComponentSystem;
-import org.terasology.entitySystem.systems.RegisterSystem;
 import org.terasology.entitySystem.systems.RegisterMode;
+import org.terasology.entitySystem.systems.RegisterSystem;
 import org.terasology.entitySystem.systems.UpdateSubscriberSystem;
-import org.terasology.logic.characters.CharacterMovementComponent;
+import org.terasology.flowingliquids.world.block.LiquidData;
 import org.terasology.logic.characters.CharacterImpulseEvent;
+import org.terasology.logic.characters.CharacterMovementComponent;
 import org.terasology.logic.location.LocationComponent;
 import org.terasology.math.JomlUtil;
-import org.terasology.math.Region3i;
 import org.terasology.math.Side;
-import org.terasology.math.geom.Vector3i;
-import org.terasology.math.geom.Vector3f;
 import org.terasology.registry.In;
 import org.terasology.world.WorldProvider;
 import org.terasology.world.block.Block;
-import org.terasology.world.block.BlockManager;
 import org.terasology.world.chunks.blockdata.ExtraBlockDataManager;
 
-import org.terasology.flowingliquids.world.block.LiquidData;
 
 /**
  * Moves characters along with the flow while they're submerged in liquid.
  */
 @RegisterSystem(RegisterMode.AUTHORITY)
 public class LiquidDragSystem extends BaseComponentSystem implements UpdateSubscriberSystem {
-    
+
     private static final Logger logger = LoggerFactory.getLogger(LiquidDragSystem.class);
-    
+
     @In
     private WorldProvider worldProvider;
-    
+
     @In
     private EntityManager entityManager;
-    
+
     @In
     private ExtraBlockDataManager extraDataManager;
     private int flowIx;
-    
+
     @Override
     public void initialise() {
         flowIx = extraDataManager.getSlotNumber("flowingLiquids.flow");
     }
-    
+
     @Override
     public void update(float delta) {
         for (EntityRef entity : entityManager.getEntitiesWith(CharacterMovementComponent.class, LocationComponent.class)) {
             float charHeight = entity.getComponent(CharacterMovementComponent.class).height;
-            Vector3f pos = entity.getComponent(LocationComponent.class).getWorldPosition();
-            int numSamples = (int) charHeight+1;
-            pos.add(0, -0.5f * charHeight * (1 + 1f/numSamples), 0);
+            Vector3f pos = entity.getComponent(LocationComponent.class).getWorldPosition(new Vector3f());
+            int numSamples = (int) charHeight + 1;
+            pos.add(0, -0.5f * charHeight * (1 + 1f / numSamples), 0);
             Vector3f force = new Vector3f();
             for (int i = 0; i < numSamples; i++) {
-                pos.add(0, charHeight/numSamples, 0);
+                pos.add(0, charHeight / numSamples, 0);
                 Vector3i blockPos = new Vector3i(pos, RoundingMode.HALF_UP);
                 Block block = worldProvider.getBlock(blockPos);
                 if (block.isLiquid()) {
-                    byte status = (byte) worldProvider.getExtraData(flowIx, blockPos);
+                    byte status = (byte) worldProvider.getExtraData(flowIx, JomlUtil.from(blockPos));
                     int rate = LiquidData.getRate(status);
                     if (rate > 0) {
                         Side direction = LiquidData.getDirection(status);
-                        Vector3f flow = direction.getVector3i().toVector3f();
-                        flow.mul(rate*block.getMass());
+                        Vector3f flow = new Vector3f(direction.direction());
+                        flow.mul(rate * block.getMass());
                         force.add(flow);
                     }
                 }
             }
-            force.mul(delta*0.4f/numSamples);
-            entity.send(new CharacterImpulseEvent(JomlUtil.from(force)));
+            force.mul(delta * 0.4f / numSamples);
+            entity.send(new CharacterImpulseEvent(force));
         }
     }
 }
